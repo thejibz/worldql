8b69a9f227838e994d1c511e6908fa433eae7942
JB <jb.roux@ymail.com>
2019-08-21 14:31:19 +0200

    Use a data only container

--- test/setup/mysql.setup.sh
+++ test/setup/mysql.setup.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-# wait for ES to start
-chmod +x /tmp/setup/wait-for-it.sh
-/tmp/setup/wait-for-it.sh localhost:3306 --timeout=30 --strict
+# wait for MySQL to start
+bash /data/worldql-core/test/setup/wait-for-it.sh localhost:3306 --timeout=30 --strict
 
 # load data in mysql
-cd /tmp/data/test_db/
-mysql -uroot -p"$MYSQL_ROOT_PASSWORD" < /tmp/data/test_db/employees.sql
+mysql -uroot -p"$MYSQL_ROOT_PASSWORD" < /data/worldql-core/test/data/mysql/test_db/employees.sql
