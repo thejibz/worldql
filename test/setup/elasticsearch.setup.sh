540bee76c647d138cdce17ca622489be3c5d5f84
JB <jb.roux@ymail.com>
2019-08-28 10:24:34 +0200

    use bash for wait-for-it

--- test/setup/elasticsearch.setup.sh
+++ test/setup/elasticsearch.setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # wait for ES to start
-sh /data/worldql-core/test/setup/wait-for-it.sh localhost:9200 --timeout=30 --strict
+bash /data/worldql-core/test/setup/wait-for-it.sh localhost:9200 --timeout=30 --strict
 
 # load data in ES
 curl -X DELETE "localhost:9200/companydatabase"
